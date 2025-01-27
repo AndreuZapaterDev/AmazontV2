585f0a28860cdc2a87fcc49dca033dbe52138d71
AndreuZapaterDev <andreuzapaterdev@gmail.com>
2025-01-27 07:09:38 +0100

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { OffersComponent } from './offers/offers.component';
+import { CategoryComponent } from './category/category.component';
+import { ProductsComponent } from './products/products.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'offers', component: OffersComponent },
+  { path: 'category', component: CategoryComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'navbar', component: NavbarComponent }
+];
