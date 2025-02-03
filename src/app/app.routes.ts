2d53e666e646fe1fc3c01baefdab1eed9eb9fd31
Alejandro Rodríguez Vallín <162721455+alejandroRV05@users.noreply.github.com>
2025-02-03 20:09:59 +0100

    Adding components and logic

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,33 @@ import { OffersComponent } from './offers/offers.component';
 import { CategoryComponent } from './category/category.component';
 import { ProductsComponent } from './products/products.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { GeneralComponent } from './general/general.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [
+      {
+        path: '',
+        component: GeneralComponent,
+      },
+      {
+        path: 'categories',
+        component: CategoriesComponent,
+        children: [
+          {
+            path: ':name',
+            component: CategoryComponent,
+          },
+        ],
+      },
+    ],
+  },
   { path: 'offers', component: OffersComponent },
   { path: 'category', component: CategoryComponent },
   { path: 'products', component: ProductsComponent },
-  { path: 'navbar', component: NavbarComponent }
+  { path: 'navbar', component: NavbarComponent },
 ];
