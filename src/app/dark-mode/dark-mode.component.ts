b6cd95bdf4720d65099fa3a46dd3b65c216be93c
Andreu Zapater <andreuzapaterdev@gmail.com>
2025-02-04 12:53:14 +0100

    Commit dark mode

--- src/app/dark-mode/dark-mode.component.ts
+++ src/app/dark-mode/dark-mode.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dark-mode',
@@ -7,11 +7,21 @@ import { Component } from '@angular/core';
   templateUrl: './dark-mode.component.html',
   styleUrls: ['./dark-mode.component.css']
 })
-export class DarkModeComponent {
+export class DarkModeComponent implements OnInit {
+  ngOnInit() {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+      document.documentElement.setAttribute('data-theme', savedTheme);
+      const input = document.getElementById('input') as HTMLInputElement;
+      input.checked = savedTheme === 'dark';
+    }
+  }
+
   toggleTheme() {
     const currentTheme = document.documentElement.getAttribute('data-theme');
     const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
     document.documentElement.setAttribute('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
   }
 
   isDarkTheme(): boolean {
