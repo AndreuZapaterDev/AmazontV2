6b2efc9720fc5205e1577d546f12fad2a250be9e
AndreuZapaterDev <andreuzapaterdev@gmail.com>
2025-03-04 07:47:09 +0100

    LAMINE YAMAL

--- src/app/featured-categories/featured-categories.component.ts
+++ src/app/featured-categories/featured-categories.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { CommonModule } from '@angular/common';
 import { Category } from '../interfaces/category.interface';
 
 @Component({
@@ -15,7 +14,13 @@ export class FeaturedCategoriesComponent implements OnInit {
   featuredCategories: Category[] = [
     { id: 1, name: 'Electrónica', category: 'electronic' },
     { id: 2, name: 'Ropa', category: 'clothes' },
-    { id: 3, name: 'Deportes', category: 'sports' }
+    { id: 3, name: 'Libros', category: 'books' },
+    { id: 4, name: 'Hogar', category: 'home' },
+    { id: 5, name: 'Juguetes', category: 'toys' },
+    { id: 6, name: 'Deportes', category: 'sports' },
+    { id: 7, name: 'Cocina', category: 'kitchen' },
+    { id: 8, name: 'Droguería', category: 'drugs' },
+    { id: 9, name: 'Juegos', category: 'games' }
   ];
   
   constructor() {}
